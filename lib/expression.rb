d966ab901e993a7b181d7884ede6f1eaca06aca7
Jesse Farmer <jesse@codeunion.io>
2014-10-03 23:27:01 -0700

    Initial commit

---
+++ lib/expression.rb
@@ -0,0 +1,17 @@
+class UndefinedVariableError < StandardError; end
+
+class Expression
+
+  # Returns a new expression
+  def initialize(expr)
+    @expr = expr
+  end
+
+  # Evaluates an expression, simplifying if possible.
+  #
+  # @param bindings [Hash] a map of variable names to concrete values
+  # @return [Expression] Returns a possibly-simplified Expression
+  def evaluate(bindings = {})
+    "Implement Expression#evaluate in #{__FILE__}"
+  end
+end

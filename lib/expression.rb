68e487af613b10fc64434c03d81b7d68c950d672
Stephens Xu <stephensxub@gmail.com>
2014-10-15 22:38:45 -0700

    added node.rb and modified expressoin.rb

--- lib/expression.rb
+++ lib/expression.rb
@@ -2,104 +2,39 @@ require "stack"
 
 class UndefinedVariableError < StandardError; end
 
-def factorial(n)
-  result = 1
-
-  n.downto(1) do |i|
-    result *= i
-  end
-
-  result
-end
-
-SYMBOL_TABLE = {
-  "+" => lambda { |stack, x, y| x + y },
-  "-" => lambda { |stack, x, y| x - y },
-  "*" => lambda { |stack, x, y| x * y },
-  "!" => lambda { |stack, n| factorial(n) },
-  "%" => lambda { |stack, n| x % y },
-  "if" => lambda { |stack, x, y, z| x == 0 ? y : z },
-  ">" => lambda { |stack, x, y| x > y ? 1 : 0 },
-  "loop" => lambda do |stack, n|
-    op = stack.pop
-    if op == "add"
-      result = 0
-      n.times do
-        result += stack.pop
-      end
-
-      result
-    elsif op == "mul"
-      result = 1
-      n.times do
-        result *= stack.pop
-      end
-
-      result
-    end
-  end
-}
-
 class Expression
 
-  # Returns a new expression
   def initialize(expr)
     @expr = expr
   end
-
-  # Evaluates an expression, simplifying if possible.
-  #
-  # @param bindings [Hash] a map of variable names to concrete values
-  # @return [Expression] Returns a possibly-simplified Expression
 
   def evaluate(bindings = {})
 
     stack = Stack.new
 
     tokens.each do |token|
-      if numeric?(token)
-        stack.push(token.to_i)
-      elsif variable?(token) && bindings[token.to_sym]
-        stack.push(bindings[token.to_sym])
-      elsif variable?(token) && !bindings[token.to_sym]
-        stack.push(token)
-        # puts "Undefined Variable #{token}"
-      elsif operator?(token)
-        stack.push(token)
-        # args = []
-
-        # (SYMBOL_TABLE[token].arity - 1).times do
-        #   args.unshift(stack.pop)
-        # end
-
-        # stack.push(call_operator(stack, token, *args))
+      if Node.operator?(token)
+        rhs = stack.pop
+        lhs = stack.pop
+        stack.push(Node.load(token, lhs, rhs))
+      elsif bindings.key?(token)
+        stack.push(Node.load(bindings[token]))
       else
-        stack.push(token)
+        stack.push(Node.load(token))
       end
     end
 
-    p stack
+    stack.pop.simplify
+
   end
 
   private
 
   def tokens
-    @expr.split(" ")
-  end
-
-  def variable?(token)
-    token =~ /[a-z]+/
-  end
-
-  def numeric?(token)
-    token =~ /^-?\d+$/
+    @tokens ||= @expr.split(" ")
   end
 
   def operator?(token)
-    SYMBOL_TABLE.key?(token)
-  end
-
-  def call_operator(stack, operator, *args)
-    SYMBOL_TABLE[operator].call(stack, *args)
+    ['+', '-', '*'].include?(token)
   end
 end
